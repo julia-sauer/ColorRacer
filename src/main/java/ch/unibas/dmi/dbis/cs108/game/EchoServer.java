aff066a21287bb51b7746bc6e092425c62cc5d11
Julia Sauer <julia.sauer@stud.unibas.ch>
2025-03-14 15:08:11 +0100

    added a welcome message for the connected client

--- src/main/java/ch/unibas/dmi/dbis/cs108/game/EchoServer.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/game/EchoServer.java
@@ -4,13 +4,25 @@ import java.io.*;
 
 public class EchoServer {
     public static void main(String[] args) {
+        int cnt = 0;
         try {
             System.out.println("Warte auf Port 8090...");
             ServerSocket echod = new ServerSocket(8090);
+            while(true) {
+                Socket socket = echod.accept();
+                EchoClientThread eC = new EchoClientThread(++cnt, socket);
+                Thread eCT = new Thread(eC);
+                eCT.start();
+            }
             Socket socket = echod.accept();
             System.out.println("Verbindung hergestellt");
+
             InputStream in = socket.getInputStream();
             OutputStream out = socket.getOutputStream();
+
+            PrintWriter writer = new PrintWriter(out, true);
+            writer.print("Hey Homie! Willkommen auf dem Server!");
+
             int c;
             while ((c = in.read()) != -1) {
                 out.write((char) c);
