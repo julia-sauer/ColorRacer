76f07134f1399d194ed7f994ad49bb746d4b825f
Julia Sauer <julia.sauer@stud.unibas.ch>
2025-03-14 14:40:01 +0100

    created EchoClientThread in EchoClient.java for the Server connection

--- src/main/java/ch/unibas/dmi/dbis/cs108/game/EchoClient.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/game/EchoClient.java
@@ -60,3 +60,32 @@ class InThread implements Runnable {
 
 }
 
+class EchoClientThread implements Runnable {
+    private int name;
+    private Socket socket;
+
+    public EchoClientThread(int name, Socket socket) { //constructor
+        this.name = name;
+        this.socket = socket;
+    }
+    public void run() {
+        String msg = "EchoServer: Verbindung " + name;
+        System.out.println(msg + " hergestellt");
+        try {
+            InputStream in = socket.getInputStream();
+            OutputStream out = socket.getOutputStream();
+            out.write(("cs108:" + msg + "\r\n").getBytes());
+            int c;
+            while((c = in.read()) != -1){
+                out.write((char) c);
+                System.out.print((char) c);
+            }
+            System.out.println("Terminate " + name);
+            socket.close();
+        }
+        catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+}
+
