8f2995b24188f066693b60d25d0fe1a24b1874ac
Jana <jana.egli@stud.unibas.ch>
2025-03-14 16:23:46 +0100

    Took InThread out of EchoClient in a separate .java file

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/game/InThread.java
@@ -0,0 +1,27 @@
+package ch.unibas.dmi.dbis.cs108.game;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class InThread implements Runnable {
+    InputStream in;
+    public InThread(InputStream in) {
+        this.in = in;
+    }
+    public void run() {
+        int len;
+        byte[] b = new byte[100];
+        try {
+            while (true) {
+                if ((len=in.read(b)) == -1) {
+                    break;
+                }
+                System.out.write(b, 0, len);
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+}
+
