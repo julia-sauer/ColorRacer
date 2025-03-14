64230c0076f7d87774461a5faf0b853bd27c818a
Jana <jana.egli@stud.unibas.ch>
2025-03-14 15:16:54 +0100

    Added maxCnt (max Connections) =  4 so while loop is not endless

--- src/main/java/ch/unibas/dmi/dbis/cs108/game/EchoServer.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/game/EchoServer.java
@@ -5,16 +5,17 @@ import java.io.*;
 public class EchoServer {
     public static void main(String[] args) {
         int cnt = 0;
+        int maxCnt = 4;
         try {
             System.out.println("Warte auf Port 8090...");
             ServerSocket echod = new ServerSocket(8090);
-            while(true) {
+            while(cnt <= maxCnt) {
                 Socket socket = echod.accept();
                 EchoClientThread eC = new EchoClientThread(++cnt, socket);
                 Thread eCT = new Thread(eC);
                 eCT.start();
             }
-            Socket socket = echod.accept();
+
             System.out.println("Verbindung hergestellt");
 
             InputStream in = socket.getInputStream();
