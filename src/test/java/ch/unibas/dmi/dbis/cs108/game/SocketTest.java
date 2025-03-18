64f3a49e24d7bdc8c4f0b46203c7307b9481b464
Jana <jana.egli@stud.unibas.ch>
2025-03-18 15:14:11 +0100

    Added folder game under src.test.java...cs108 and added SocketTest in there

---
+++ src/test/java/ch/unibas/dmi/dbis/cs108/game/SocketTest.java
@@ -0,0 +1,22 @@
+package ch.unibas.dmi.dbis.cs108.game;
+import java.net.*;
+import java.io.*;
+
+public class SocketTest {
+    public static void main(String[] args){
+        try{
+            Socket sock = new Socket(args[0], 8090);
+            InputStream in = sock.getInputStream();
+            int len;
+            byte[] b = new byte[100];
+            while((len = in.read(b)) != -1) {
+                System.out.write(b, 0, len);
+            }
+            in.close();
+            sock.close();
+        } catch (IOException e){
+            System.err.println(e.toString());
+            System.exit(1);
+        }
+    }
+}
