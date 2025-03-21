4856cb230dce86fa4656621129b891adffcb4b0e
Julia Sauer <julia.sauer@stud.unibas.ch>
2025-03-21 19:11:43 +0100

    added JavaDoc to InThread and also increased the the buffer size for bigger messages

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/InThread.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/InThread.java
@@ -1,23 +1,38 @@
 package ch.unibas.dmi.dbis.cs108.client;
 
 import java.io.*;
+
+/**
+ * Eine lauffähige Klasse, die Daten aus einem InputStream liest und sie in die Standardausgabe schreibt.
+ * Diese Klasse soll in einem separaten Thread verwendet werden, um kontinuierlich von einem Netzwerksocket zu lesen.
+ */
 class InThread implements Runnable {
-    InputStream in;
+    private InputStream in;
+
+    /**
+     * Konstruiert einen InThread mit dem angegebenen InputStream.
+     *
+     * @param in den InputStream, aus dem gelesen werden soll
+     */
     public InThread(InputStream in) {
         this.in = in;
     }
+
+    /**
+     * Liest Daten aus dem InputStream und schreibt sie in die Standardausgabe.
+     * Diese Methode läuft in einer Schleife und liest kontinuierlich Bytes aus dem InputStream
+     * bis das Ende des Streams erreicht ist (angezeigt durch einen Rückgabewert von -1 von read()).
+     */
     public void run() {
-        int len;
-        byte[] b = new byte[100];
+        byte[] buffer = new byte[1024]; // Increased buffer size
+        int bytesRead;
         try {
-            while (true) {
-                if ((len=in.read(b))== -1) {
-                    break;
-                }
-                System.out.write(b, 0, len);
+            while ((bytesRead = in.read(buffer)) != -1) {
+                System.out.write(buffer, 0, bytesRead);
+                System.out.flush(); // Ensure output is flushed
             }
+        } catch (IOException e) {
+            System.err.println("ERROR: " + e.toString());
         }
-        catch (IOException e) {
-            System.err.println("ERROR: " + e.toString()); }
     }
 }
