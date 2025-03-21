c2d015d9b8d6e6664d6b153fa2aa3d81d7fd8aba
Jana <jana.egli@stud.unibas.ch>
2025-03-21 21:42:26 +0100

    Implemented the Server so that multiple Clients can log on to the Server an the Clients get numbered

---
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/ClientHandler.java
@@ -0,0 +1,31 @@
+package ch.unibas.dmi.dbis.cs108.server;
+
+import java.io.*;
+import java.net.*;
+
+public class ClientHandler implements Runnable {
+    private final Socket clientSocket;
+    private final int clientNumber;
+
+    public ClientHandler(int clientNumber, Socket socket) {
+        this.clientNumber = clientNumber;
+        this.clientSocket = socket;
+    }
+
+    public void run() {
+        try {
+            InputStream in = clientSocket.getInputStream();
+            OutputStream out = clientSocket.getOutputStream();
+            int c;
+            while ((c = in.read()) != -1) {
+                out.write((char)c);
+                System.out.print((char)c);
+            }
+            System.out.println("Connection closed for Client " + clientNumber);
+            clientSocket.close();
+        }
+        catch (IOException e) {
+            System.err.println("Fehler bei Client " + clientNumber + ": " + e.getMessage());
+        }
+    }
+}
--- src/main/java/ch/unibas/dmi/dbis/cs108/server/Server.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/Server.java
@@ -1,4 +1,44 @@
 package ch.unibas.dmi.dbis.cs108.server;
 
+import java.net.*;
+import java.io.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
 public class Server {
+    private static final AtomicInteger activeClients = new AtomicInteger(0);
+    private static final int maxClients = 4;
+    /**
+     * Startet einen Server, der auf Verbindungen wartet und stellt Netzwerkverbindung
+     * von Client zu Server dar.
+     * ServerSocket erstellt einen Server (hier: echod) der auf Port 8090 läuft.
+     * echod.accept(); wartet bis sich Client verbindet.
+     * Sobald sich ein Client verbunden hat wird "Connection established" ausgegeben.
+     *
+     * while-Schleife: Solange bis Client Verbindung beendet. Speichert was von Client kommt in c
+     * und gibt genau das Gleiche zurück (out.write).
+     * Wenn Client Verbindung beendet geht es aus while-Schleife, dann wir Verbindung zu Client beendet
+     * (socket.close) dann wird Server geschlossen (echod.close).
+     * @param args
+     */
+    public static void main(String[] args) {
+        int cnt = 0;
+        try {
+            System.out.println("Waiting for port 8090...");
+            ServerSocket echod = new ServerSocket(8090);
+
+            while (true) {
+                Socket clientSocket = echod.accept();
+                System.out.println("Connection established");
+                ClientHandler cH = new ClientHandler(++cnt, clientSocket);
+                Thread cHT = new Thread(cH);
+                cHT.start();
+            }
+            
+        } catch (IOException e) {
+            System.err.println(e.toString());
+            System.exit(1);
+        }
+    }
 }
+
