765aac86cab84ab99e65abf96070bc009491ec93
Julia Sauer <julia.sauer@stud.unibas.ch>
2025-03-21 19:09:48 +0100

    made comments where I need to implement methods for the nickname and chat

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/Client.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/Client.java
@@ -9,10 +9,12 @@ public class Client {
             Socket sock = new Socket(args[0], Integer.parseInt(args[1]));
             InputStream in = sock.getInputStream();
             OutputStream out= sock.getOutputStream();
+
             // create server reading thread
             InThread th = new InThread(in);
             Thread iT = new Thread(th);
             iT.start();
+
             // stream input
             BufferedReader conin = new BufferedReader(new InputStreamReader(System.in));
             String line = " ";
@@ -20,16 +22,20 @@ public class Client {
                 // reading input stream
                 line = conin.readLine();
                 if (line.equalsIgnoreCase("QUIT")) {
+                    //sendQuitMessage(); odr so halt eifach das vom ProtocolReaderClient
                     break;
+                } else if (line.startsWith("/nick")){
+                    //changeNickname(line.substring(6)); oder das vom Protocol
+                } else {
+                    //sendMessage(line); oder das vom Protocol
                 }
-                // writing to ECHO server
-                out.write(line.getBytes());
-                out.write("\r\n".getBytes());
-            } // terminate program
-            System.out.println("terminating ..");
-            in.close(); out.close(); sock.close();
-        }
-        catch (IOException e) {
+            }
+            // terminate program
+            System.out.println("Terminating ...");
+            in.close();
+            out.close();
+            sock.close();
+        } catch (IOException e) {
             System.err.println("Error: " + e.toString());
         }
     }
