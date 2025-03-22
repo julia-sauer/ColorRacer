07b7cc826f48f97f237ac636e8cd9dc4b847d863
Jana <jana.egli@stud.unibas.ch>
2025-03-22 22:19:21 +0100

    Added readCommand method

--- src/main/java/ch/unibas/dmi/dbis/cs108/network/ProtocolReaderServer.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/network/ProtocolReaderServer.java
@@ -5,11 +5,21 @@ import ch.unibas.dmi.dbis.cs108.server.UserList;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Die Klasse {@code ProtocolReaderServer} liest eingehende Nachrichten vom Client
  * und leitet sie an den Server weiter.
  */
 public class ProtocolReaderServer {
-
+    public static String readCommand(InputStream in) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            return reader.readLine();
+        } catch (IOException e) {
+            System.err.println("Error in reading command");
+            return null;
+        }
+    }
 }
