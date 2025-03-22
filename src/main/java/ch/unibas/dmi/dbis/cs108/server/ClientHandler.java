c6efd6feeab6b1c5229491ff4e943d1f28c8dc16
milo <flo.hansen@stud.unibas.ch>
2025-03-22 12:43:41 +0100

    Added a method to remove user from user list  in the ckienthandler and called the method in the run method

--- src/main/java/ch/unibas/dmi/dbis/cs108/server/ClientHandler.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/server/ClientHandler.java
@@ -32,11 +32,19 @@ public class ClientHandler implements Runnable {
             System.out.println("Connection closed for Client " + clientNumber);
             clientSocket.close();
             PingThread.stopPinging();
+            removeUser(clientNumber); // Aufruf der Methode removeUser
             Server.ClientDisconnected();
 
         }
         catch (IOException e) {
             System.err.println("Fehler bei Client " + clientNumber + ": " + e.getMessage());
         }
+    }
+    /**
+     * Entfernt einen Benutzer aus der Benutzerliste.
+     * @param clientNumber Die ID des zu entfernenden Benutzers.
+     */
+    private void removeUser(int clientNumber) {
+        UserList.removeUser(clientNumber);
     }
 }
