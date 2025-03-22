8770f01a44c63ecbc6467b0fe8ca99192722070c
AnaSvalin <ana.svalina2005@gmail.com>
2025-03-22 14:31:23 +0100

    Added JavaDoc to the class WriterClient and explained the use of this class.

--- src/main/java/ch/unibas/dmi/dbis/cs108/network/ProtocolWriterClient.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/network/ProtocolWriterClient.java
@@ -1,4 +1,16 @@
 package ch.unibas.dmi.dbis.cs108.network;
 
+/**
+ * Die Klasse ProtocolWriterClient wandelt die Spieler Eingaben
+ * (die im Terminal von den Spieler*innen eingegeben werden kann)
+ * in die entsprechenden Protokollbefehle um, die in den Commands definiert sind
+ * und sendet sie wenn nötig an den Server weiter.
+ * Die Spieleingabe und die Umwandlung in das entsprechende Protokollbefehl aufgelistet:
+ * connect -> JOIN
+ * leave -> QUIT
+ * message -> CHAT
+ * nicknamechange -> NICK
+ *
+ */
 public class ProtocolWriterClient {
 }
