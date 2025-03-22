193b10822a79aaa58df54726e9b7107003205468
AnaSvalin <ana.svalina2005@gmail.com>
2025-03-22 17:16:09 +0100

    explained what the class ProtocolReaderServer is, for javaDoc.

--- src/main/java/ch/unibas/dmi/dbis/cs108/network/ProtocolReaderServer.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/network/ProtocolReaderServer.java
@@ -1,4 +1,15 @@
 package ch.unibas.dmi.dbis.cs108.network;
 
+import ch.unibas.dmi.dbis.cs108.server.Server;
+import ch.unibas.dmi.dbis.cs108.server.UserList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * Die Klasse {@code ProtocolReaderServer} liest eingehende Nachrichten vom Client
+ * und leitet sie an den Server weiter.
+ */
 public class ProtocolReaderServer {
+
 }
