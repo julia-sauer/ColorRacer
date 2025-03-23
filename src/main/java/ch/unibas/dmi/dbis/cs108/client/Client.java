871e7e1c7539e900906f7e4c8249691aa378bfd3
AnaSvalin <ana.svalina2005@gmail.com>
2025-03-23 23:15:34 +0100

    Added an else if statment for the terminal command connect and added another else if statement for the case 'unknown command' and changed the terminal command QUIT to leave

--- src/main/java/ch/unibas/dmi/dbis/cs108/client/Client.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/client/Client.java
@@ -50,7 +50,7 @@ public class Client {
             String line = " ";
             while (true) {
                 line = conin.readLine();
-                if (line.equalsIgnoreCase("QUIT ")) {
+                if (line.equalsIgnoreCase("leave")) {
                     // Verbindung beendet
                     System.out.println("Terminating ...");
                     in.close();
@@ -60,9 +60,17 @@ public class Client {
                 } else if (line.startsWith("nicknamechange")){
                     // Überprüft, ob Benutzer nicknamechange eingegeben hat.
                     protocolClient.changeNickname(line.substring(15), out); //sollte im WriterClient implementiert sein
-                } else {
-                    // Sendet eine Chat-Nachricht an den Server, die dann an alle verbundenen Clients weitergeleitet wird.
-                    protocolClient.sendChat(line);
+                } else if (line.startsWith("connect ")) {
+                    // JOIN-Befehl senden
+                    String nickname = line.substring(8).trim();
+                    protocolClient.sendJoin(nickname);
+                } else if (line.startsWith("message ")) {
+                    // Chatnachricht senden
+                String message = line.substring(8).trim();
+                protocolClient.sendChat(message);
+                } else { // ungültiger Befehl
+                System.out.println("Unknown command. Use: connect | nicknamechange | message | QUIT");
+
                 }
             }
             // Programm beenden
