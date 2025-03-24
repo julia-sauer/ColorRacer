92031bff9fce17dbe09c48b8867b53f58c3181c6
AnaSvalin <ana.svalina2005@gmail.com>
2025-03-24 01:03:22 +0100

    creaded new class UserListJoinTest to thest the JOIN command. Added nessecary improts.

---
+++ src/test/java/ch/unibas/dmi/dbis/cs108/gamenew/UserListJoinTest.java
@@ -0,0 +1,15 @@
+package ch.unibas.dmi.dbis.cs108.gamenew;
+
+package ch.unibas.dmi.dbis.cs108.server;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserListJoinTest {
+
+}
