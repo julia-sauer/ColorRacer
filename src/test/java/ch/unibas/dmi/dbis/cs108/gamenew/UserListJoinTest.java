837dd21460a5769df02aa990cb23262b5f486f97
AnaSvalin <ana.svalina2005@gmail.com>
2025-03-24 01:10:09 +0100

    added another import and testing if there are suffix duplicates

--- src/test/java/ch/unibas/dmi/dbis/cs108/gamenew/UserListJoinTest.java
+++ src/test/java/ch/unibas/dmi/dbis/cs108/gamenew/UserListJoinTest.java
@@ -1,6 +1,9 @@
 package ch.unibas.dmi.dbis.cs108.gamenew;
 
-package ch.unibas.dmi.dbis.cs108.server;
+import ch.unibas.dmi.dbis.cs108.server.Server;
+import ch.unibas.dmi.dbis.cs108.server.UserList;
+import ch.unibas.dmi.dbis.cs108.server.User;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,4 +28,34 @@ public class UserListJoinTest {
         assertNotNull(user);
         assertEquals("Anna", user.getNickname());
     }
+    @Test
+    public void testDuplicateNicknameGetsSuffix() {
+        OutputStream out1 = new ByteArrayOutputStream();
+        OutputStream out2 = new ByteArrayOutputStream();
+
+        UserList.addUser("Anna", out1);
+
+        String nickname = "Anna";
+        String finalNick = nickname;
+        int suffix = 1;
+        while (UserList.containsUserName(finalNick)) {
+            finalNick = nickname + suffix++;
+        }
+
+        int userId2 = UserList.addUser(finalNick, out2);
+        User user2 = UserList.getUser(userId2);
+
+        assertEquals("Anna1", user2.getNickname());
+    }
+
+    @Test
+    public void testNicknameExists() {
+        OutputStream dummyOut = new ByteArrayOutputStream();
+        UserList.addUser("Max", dummyOut);
+
+        assertTrue(UserList.containsUserName("Max"));
+        assertFalse(UserList.containsUserName("Moritz"));
+    }
+
+
 }
