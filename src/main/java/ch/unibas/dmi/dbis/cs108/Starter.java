b6167178cfd2b6cd5955fe116a0635e1030aa421
Jana <jana.egli@stud.unibas.ch>
2025-04-03 21:24:28 +0200

    Implemented ifpart for input server

--- src/main/java/ch/unibas/dmi/dbis/cs108/Starter.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/Starter.java
@@ -1,8 +1,25 @@
 package ch.unibas.dmi.dbis.cs108;
 
+import ch.unibas.dmi.dbis.cs108.client.Client;
+import ch.unibas.dmi.dbis.cs108.server.Server;
 /**
  * This method starts either the Server or the Client
  */
 public class Starter {
+    public static void main(String[] args) {
+        try {
+            String input = args[0];
+            if (input.equalsIgnoreCase("server")) {
+                int port = Integer.parseInt(args[1]);
+                Server server = new Server(port);
+                server.start();
+            } else if (input.equalsIgnoreCase("client")) {
 
+            }
+        } catch (Exception e) {
+            System.out.println("Your input was incorrect. Please try again! \n\n"
+                    + "It has to be done as followed: \n"
+                    + "client <hostadress>:<port> [<username>] | server <port> \n\n");
+        }
+    }
 }
