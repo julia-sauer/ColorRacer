73402f65c65e414050e42f406c4c0e5478f04111
Jana <jana.egli@stud.unibas.ch>
2025-04-03 22:09:22 +0200

    Implemented the client part in the Starter-Method

--- src/main/java/ch/unibas/dmi/dbis/cs108/Starter.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/Starter.java
@@ -2,10 +2,14 @@ package ch.unibas.dmi.dbis.cs108;
 
 import ch.unibas.dmi.dbis.cs108.client.Client;
 import ch.unibas.dmi.dbis.cs108.server.Server;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 /**
  * This method starts either the Server or the Client
  */
 public class Starter {
+    //public static final Logger LOGGER = LogManager.getLogger();
+
     public static void main(String[] args) {
         try {
             String input = args[0];
@@ -13,8 +17,18 @@ public class Starter {
                 int port = Integer.parseInt(args[1]);
                 Server server = new Server(port);
                 server.start();
+                //LOGGER.info("Server started");
             } else if (input.equalsIgnoreCase("client")) {
-
+                String[] hostPort = args[1].split(":");
+                String host = hostPort[0];
+                int port = Integer.parseInt(hostPort[1]);
+                String username = null;
+                if(args.length > 2) {
+                    username = args[2];
+                }
+                Client client = new Client(host, port, username);
+                client.start();
+                //LOGGER.info("Client started");
             }
         } catch (Exception e) {
             System.out.println("Your input was incorrect. Please try again! \n\n"
