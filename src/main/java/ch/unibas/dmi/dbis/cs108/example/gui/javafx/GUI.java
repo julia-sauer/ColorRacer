1f070cbcf207ca197febae40d03b7ea5413ef3c2
julia <julia.sauer@stud.unibas.ch>
2025-04-05 18:29:46 +0200

    I implemented the start method again from scratch because before it didn't work at all. The old implementation is still there in a comment.

--- src/main/java/ch/unibas/dmi/dbis/cs108/example/gui/javafx/GUI.java
+++ src/main/java/ch/unibas/dmi/dbis/cs108/example/gui/javafx/GUI.java
@@ -1,43 +1,62 @@
 package ch.unibas.dmi.dbis.cs108.example.gui.javafx;
 
+import ch.unibas.dmi.dbis.cs108.client.Client;
 import ch.unibas.dmi.dbis.cs108.gui.ChatController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
+
 
 /**
  * This is an example JavaFX-Application.
  */
 public class GUI extends Application {
 
+    private static Client client; // This will be set before launching the GUI.
+
+    public static void setClient(Client clientInstance) {
+        client = clientInstance;
+    }
     /**
-     * Launching this method will not work on some platforms.
-     * What you should do is to create a separate main class and launch the GUI class from there as is done in {@link Main}
+     * Launches the GUI with the ChatTemplate
      */
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        URL fmxl = getClass().getResource("/ChatTemplate.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(fmxl);
-        TitledPane chatPane = fxmlLoader.load();
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        // Scene scene = new Scene(new StackPane(l), 640, 480);
-        Scene scene = new Scene(chatPane, 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) throws IOException {
+//        String javaVersion = System.getProperty("java.version");
+//        String javafxVersion = System.getProperty("javafx.version");
+//
+//        URL fmxl = getClass().getResource("/ChatTemplate.fxml");
+//        FXMLLoader fxmlLoader = new FXMLLoader();
+//        fxmlLoader.setLocation(fmxl);
+//        TitledPane chatPane = fxmlLoader.load();
+//
+//        Scene scene = new Scene(chatPane, 440, 680);
+//        stage.setScene(scene);
+//        stage.show();
+
+        // Load the FXML file (ensure the correct resource path is used)
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ChatTemplate.fxml"));
+        Parent root = loader.load();
+
+        // Retrieve the ChatController from the FXML loader.
+        ChatController controller = loader.getController();
+
+        // Inject the ProtocolWriterClient into the ChatController.
+        controller.setProtocolWriter(client.getProtocolWriter());
+        // Inject the ChatController into the client (or its ProtocolReaderClient)
+        client.setChatController(controller);
+
+        // Setup and show the GUI.
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Chat");
+        primaryStage.show();
     }
 
 }
